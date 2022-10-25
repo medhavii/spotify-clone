7294bdf536a679bf64437f04c7419fd97e203c2e
medhavii <medhavimehta118@gmail.com>
2022-10-25 20:42:14 +0530

    update

---
+++ script.js
@@ -0,0 +1,18 @@
+let songs = [
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+    {songName: "Salen-e-ishq", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"}
+]
+let audioElement = new Audio('songs/1.mp3');
+//audioElement.play();
+
+
+//Listen to Events
+document.addEventListener('time')
